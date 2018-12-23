8771ee3a35ff0202b2dd1d6b5f590ce5b54f0f95
dantemgs <gold_chord@mail.ru>
2018-12-23 16:41:35 +0400

    1-3 PHPdoc finish

--- 1.php
+++ 1.php
@@ -4,27 +4,56 @@
 
 /**
  * Class Product
- * @param {int} $id this is the identifier.
- * @param {string} $name this is the name product.
- * @param {int} $price this is the price.
  */
 class Product {
     protected $id;
     protected $name;
     protected $price;
 
-    function __construct()
+    /**
+     * Product constructor.
+     * @param null {int} $id this is the identifier.
+     * @param null {string} $name this is the name product.
+     * @param null {int} $price this is the price.
+     */
+    function __construct($id = null, $name = null, $price = null)
     {
-        $this->id;
-        $this->name;
-        $this->price;
+        $this->id = $id;
+        $this->name = $name;
+        $this->price = $price;
     }
 
     /**
      * Render block of products.
-     * @return string block of products.
+     * @return string return block of products.
      */
     public function render(){
         return "<div id='{$this->id}'><h3>{$this->name}</h3><p>Цена: {$this->price}</p></div>";
+    }
+}
+
+class DiscountedProduct extends Product {
+    protected $discount;
+    protected $discountedPrice;
+
+    /**
+     * DiscountedProduct constructor.
+     * @param null {int} $id this is the identifier.
+     * @param null {string} $name this is the name product.
+     * @param null {int} $price this is the price.
+     * @param null {proc} $discount.
+     */
+    function __construct($id = null, $name = null, $price = null, $discount = null)
+    {
+        parent::__construct($id, $name, $price);
+        $this->discount = $discount;
+        $this->discountedPrice = $this->calculateDiscountedPrice();
+    }
+
+    /**
+     * @return float|int return discounted price.
+     */
+    protected function calculateDiscountedPrice(){
+         return $this->price*(1 - $this->discount/100);
     }
 }
